bf96c70a32d2b07b0e14c8c02eb37441e0ff0c43
zhangyang08 <zhangyang08@qiyi.com>
2023-03-01 19:44:47 +0800

    add protocal

--- nim_in_action/chatapp/src/protocal.nim
+++ nim_in_action/chatapp/src/protocal.nim
@@ -1,9 +1,36 @@
+import json
 type
     Message* = object
-        usrname*: string
+        username*: string
         message*: string
 
 proc parseMessage*(data: string): Message =
-    discard
+    let dataJson = parseJson(data)
+    result.username = dataJson["username"].getStr()
+    result.message = dataJson["message"].getStr()
 
+proc createMessage*(username:string, message: string): string =
+    result = $(
+        %{"username": %username, "message": %message}
+    ) & "\c\l"
+
+when isMainModule:
+    block:
+        let data = """{"username": "John", "message": "Hi!"}"""
+        let parsed = parseMessage(data)
+        doAssert(parsed.username == "John")
+        doAssert(parsed.message == "Hi!")
+    block:
+        let data = """barfoo"""
+        try:
+            let parsed = parseMessage(data)
+            doAssert false
+        except JsonParsingError:
+            # echo("err:" & getCurrentExceptionMsg())
+            doAssert true
+        except:
+            doAssert false
+    block:
+        let expected = """{"username":"dom","message":"hello"}""" & "\c\l"
+        doAssert(createMessage("dom", "hello") == expected)
 
