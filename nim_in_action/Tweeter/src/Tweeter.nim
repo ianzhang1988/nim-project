6d70e772b962c43e16938c6c4128b981e684e61d
zhangyang08 <zhangyang08@qiyi.com>
2023-05-12 16:56:16 +0800

    add view

--- nim_in_action/Tweeter/src/Tweeter.nim
+++ nim_in_action/Tweeter/src/Tweeter.nim
@@ -1,8 +1,23 @@
-import asyncdispatch
+import asyncdispatch, times
 import jester
+import database, views/user, views/general
+
+let db = newDatabase()
 
 routes:
   get "/":
-    resp "hello world!"
-  
+    if request.cookies.hasKey("username"):
+      var user: User
+      if not db.findUser(request.cookies["username"], user):
+        user = User(username: request.cookies["username"], following: @[])
+        db.create(user)
+      let messages = db.findMessages(user.following)
+      resp renderMain(renderTimeline(user.username, messages))
+    else:
+      resp renderMain(renderLogin())
+
+  post "/login":
+    setCookie("username", @"username", now() + 2.hours)
+    redirect("/")
+
 runForever()
