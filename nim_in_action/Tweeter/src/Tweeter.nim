75ed49d2cf0da9b2553f28e0c5833f9295acae32
zhangyang08 <zhangyang08@qiyi.com>
2023-05-17 19:17:52 +0800

    add createMessage and user page

--- nim_in_action/Tweeter/src/Tweeter.nim
+++ nim_in_action/Tweeter/src/Tweeter.nim
@@ -11,7 +11,7 @@ routes:
       if not db.findUser(request.cookies["username"], user):
         user = User(username: request.cookies["username"], following: @[])
         db.create(user)
-      let messages = db.findMessages(user.following)
+      let messages = db.findMessages(user.following & user.username)
       resp renderMain(renderTimeline(user.username, messages))
     else:
       resp renderMain(renderLogin())
@@ -19,5 +19,23 @@ routes:
   post "/login":
     setCookie("username", @"username", now() + 2.hours)
     redirect("/")
+
+  post "/createMessage":
+    let message = Message(
+      username: @"username",
+      time: getTime(),
+      msg: @"message"
+    )
+    db.post(message)
+    redirect("/")
+
+  get "/@name":
+    cond '.' notin @"name"
+    var user: User
+    if not db.findUser(@"name", user):
+      halt "User not Found"
+    
+    let messages = db.findMessages(@[user.username])
+    resp renderMain( renderUser(user) & renderMessages(messages))
 
 runForever()
